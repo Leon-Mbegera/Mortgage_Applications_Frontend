a2ac3ebe77aba8381d576e11285d95fd0816f439
Leon-Mbegera <mbegeraleon89@gmail.com>
2025-10-06 12:08:02 +0300

    define api methods for user authentication and managing applications

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create(
+const apiClient = axios.create(
   {
     baseUrl: "http://localhost:3000",
     headers: {
@@ -9,7 +9,7 @@ const api = axios.create(
   }
 );
 
-api.interceptors.request.use((config) => {
+apiClient.interceptors.request.use((config) => {
   const token = localStorage.getItem('morgage-portal-token');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
@@ -17,3 +17,29 @@ api.interceptors.request.use((config) => {
 
   return config;
 })
+
+export const register = (username, email, password) => {
+  apiClient.post("/api/auth/register", { username, email, password })
+}
+
+export const login = (username, password) => {
+  apiClient.post("/api/auth/login", { username, password})
+}
+
+export const submitApplication = (applicationData) => {
+  apiClient.post("/api/applications", applicationData)
+}
+
+export const getUserApplications = () => {
+  apiClient.get("/api/applications")
+}
+
+export const getAllApplications = () => {
+  apiClient.get("/api/admin/applications")
+}
+
+export const updateApplicationStatus = (id, status) => {
+  apiClient.put(`/api/admin/applications/${id}`, { status })
+}
+
+export default apiClient;
