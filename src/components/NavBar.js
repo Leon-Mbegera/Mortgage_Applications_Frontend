d790d5d6e150e6fb99dde27c0541a990b5c08829
Leon-Mbegera <mbegeraleon89@gmail.com>
2025-10-06 12:52:30 +0300

    build NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,40 @@
+import { Link, useNavigate } from 'react-router-dom';
+import { isAuthenticated, getUserRole, removeToken } from "../utils/auth";
+
+const NavBar = () => {
+  const navigate = useNavigate();
+  const isAuthenticated = isAuthenticated();
+  const role = getUserRole();
+
+  const handleLogout = () => {
+    removeToken();
+    navigate('/login');
+  }
+
+  return (
+    <nav className="bg-gray-800 p-4 text-white">
+      <div className="container mx-auto flex justify-between">
+        <Link to="/"className="text-xl font-bold">Mortgage Portal</Link>
+      </div>
+      <div>
+        {
+          isAuthenticated ? (
+            <>
+              {role === 'USER' && <Link to="/user_dashboard" className="mr-4">User Dashboard</Link>}
+              {role === 'ADMIN' && <Link to="/admin_dashboard" className="mr-4">Admin Dashboard</Link>}
+              <button onClick={handleLogout} className="bg-red-500 px-4 py-2 rounded">
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <Link to="/login" className="mr-4">Login</Link>
+              <Link to="/register">Register</Link>
+            </>
+        )}
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar;
