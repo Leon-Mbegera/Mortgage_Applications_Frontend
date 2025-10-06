3b6f7942325c1a42f95d4409f95fd2e41f02397f
Leon-Mbegera <mbegeraleon89@gmail.com>
2025-10-06 12:27:03 +0300

    define utility to extract user role from token

--- src/services/utils/auth.js
+++ src/services/utils/auth.js
@@ -1,7 +1,20 @@
-const setToken = (token) => localStorage.setItem('morgage-portal-token', token);
+export const setToken = (token) => localStorage.setItem('morgage-portal-token', token);
 
-const getToken = () => localStorage.getItem('morgage-portal-token');
+export const getToken = () => localStorage.getItem('morgage-portal-token');
 
-const removeToken = () => localStorage.removeItem('morgage-portal-token');
+export const removeToken = () => localStorage.removeItem('morgage-portal-token');
 
-const isAuthenticated = () => !!getToken();
+export const isAuthenticated = () => !!getToken();
+
+export const getUserRole = () => {
+  const token = getToken();
+  if (!token) return null;
+
+  try {
+    const payload = JSON.parse(atob(token.split('.')[1]));
+    return payload.roles.includes('ROLE_ADMIN') ? 'ADMIN' : 'USER'
+  } catch (error) {
+    console.log('role extraction from token error', error)
+    return null;
+  }
+}
